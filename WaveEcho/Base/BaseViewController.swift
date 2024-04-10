3133bf67ea7472a2a822b4e2538b19ad1ecb675a
jieun <jieun_0330@naver.com>
2024-04-11 00:05:23 +0900

    feat: BaseViewController 구현

---
+++ WaveEcho/Base/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  WaveEcho
+//
+//  Created by 박지은 on 4/11/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class BaseViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        configureHierarchy()
+        configureConstraints()
+        configureView()
+        bind()
+    }
+    
+    func configureHierarchy() { }
+    func configureConstraints() { }
+    func configureView() { }
+    func bind() { }
+}
