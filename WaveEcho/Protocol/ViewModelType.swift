a7d44f66694a6da19dcd6281ee17ae796ec54154
jieun <jieun_0330@naver.com>
2024-04-11 00:07:24 +0900

    feat: ViewModelType 구현

---
+++ WaveEcho/Protocol/ViewModelType.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModelType.swift
+//  WaveEcho
+//
+//  Created by 박지은 on 4/11/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol ViewModelType {
+    associatedtype Input
+    associatedtype Output
+    
+    var disposeBag: DisposeBag { get set }
+    
+    func transform(input: Input) -> Output
+}
